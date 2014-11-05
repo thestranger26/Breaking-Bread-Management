023db653611990c015269135b46b2223bd6bbfa6
thestranger26 <thestranger26@hotmail.fr>
2014-11-05 10:00:47 +0100

    Init Projets
    Commit des fichiers créés par Netbeans

---
+++ BBM_client/src/bbm_client/BBM_client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbm_client;
+
+/**
+ *
+ * @author Tristan
+ */
+public class BBM_client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ BBM_serveur/src/bbm_serveur/BBM_serveur.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbm_serveur;
+
+/**
+ *
+ * @author Tristan
+ */
+public class BBM_serveur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
