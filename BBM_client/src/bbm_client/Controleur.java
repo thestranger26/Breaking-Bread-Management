78b13d3a9266edcce7796f6e46e664abb513532f
lrinaudo <t.zicara@gmail.com>
2014-11-25 13:50:44 +0100

    Création de la méthode commencerCuisson() dans Controleur

--- BBM_client/src/bbm_client/Controleur.java
+++ BBM_client/src/bbm_client/Controleur.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 /*
  * To change this template, choose Tools | Templates
@@ -106,10 +107,57 @@ public class Controleur implements Serializable {
         return ret;
         
     }
-    public void commencerCuisson(Fournee fournee) {
+   public void commencerCuisson(Fournee fournee) {
         fournee.setEtatFournee(ListeEtatsFournee.EnCuisson);
         fournee.getDebutCuisson();
     }
     
     
 }
+=======
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbm_client;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import javax.swing.GroupLayout;
+
+/**
+ *
+ * @author Tristan
+ */
+public class Controleur implements Serializable {
+    private Utilisateur utilisateur;
+    private VuePrincipale vue;
+    private ArrayList<Produit> listeProduits;
+    private HashMap<Produit,Integer> factureEnCours;
+    
+    public Controleur() {
+        System.out.println("OK");
+        vue = new IHMManageur();
+
+        
+        
+    //-----------------------MAXIME S-----------------------//
+
+        Viennoiserie p3 = new Viennoiserie(ListeViennoiseries.Croissant, 30, 2, 20, null);
+        p3.getInfosfounnee(123);
+        //listeProduits.add(p1);
+        //listeProduits.add(p2);
+    //-----------------------MAXIME S FIN-----------------------//
+        
+//vue.setVisible(true);
+       
+        
+        //vue.setVisible(true);
+        listeProduits = new ArrayList<Produit>();
+        factureEnCours = new HashMap<Produit,Integer>();
+    }
+   
+}
+>>>>>>> Stashed changes
